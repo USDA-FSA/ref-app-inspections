fbd469155ef6d6958bcb015fce68558ab017157c
Francis Rupert <francisrupert@gmail.com>
2018-08-06 15:11:59 -0500

    scripted behavior for filter by status column [amends #4]

--- src/js/ref-app-inspections.js
+++ src/js/ref-app-inspections.js
@@ -1,1 +1,22 @@
+
 console.log('ref-app-inspections.js loaded');
+
+$('body').on('change', '[data-behavior~="inspections-filter"]', function(event) {
+
+  var $self = $(this);
+  var $target = $('#' + $self.attr('data-filter-target'))
+  var newStatus = $self.val();
+
+  $target
+    .removeClass('fic-inspections--status-filter-is-all fic-inspections--status-filter-is-complete fic-inspections--status-filter-is-assigned fic-inspections--status-filter-is-logged fic-inspections--status-filter-is-in-progress fic-inspections--status-filter-is-rejected ')
+    .addClass('fic-inspections--status-filter-' + newStatus)
+  ;
+
+  if ($target.hasClass('fic-inspections--status-filter-is-all')) {
+    $('#inspections-pagination').removeAttr('hidden');
+  }
+  else {
+    $('#inspections-pagination').attr('hidden', 'true');
+  }
+
+})
