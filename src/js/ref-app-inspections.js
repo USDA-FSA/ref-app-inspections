f10207bd472b6015818561390792b444bb0a61f2
Francis Rupert <francisrupert@gmail.com>
2018-08-07 07:31:05 -0500

    scripted behavior for Inpsections checkbox/selection [amends #4]

--- src/js/ref-app-inspections.js
+++ src/js/ref-app-inspections.js
@@ -20,3 +20,34 @@ $('body').on('change', '[data-behavior~="inspections-filter"]', function(event) 
   }
 
 })
+
+$('body').on('change', '[data-behavior~="inspections-select-row"]', function(event) {
+
+  var $self = $(this);
+  var $row = $self.closest('.fic-inspections__row');
+
+  $row.toggleClass('fic-inspections__row--selected');
+
+})
+
+$('body').on('change', '[data-behavior~="inspections-select-all"]', function(event) {
+
+  var $self = $(this);
+  var $row = $self.closest('.fic-inspections__row--thead');
+  var $table = $self.closest('.fic-inspections');
+  var $rowsAll = $table.find('.fic-inspections__row');
+  var $rowsAllChecks = $rowsAll.find('.fsa-checkbox');
+  var isChecked = $self.is(':checked')
+
+  if(isChecked) {
+    $rowsAll.addClass('fic-inspections__row--selected');
+    $rowsAllChecks.prop('checked', true);
+  }
+  else {
+    $rowsAll.removeClass('fic-inspections__row--selected');
+    $rowsAllChecks.prop('checked', false);
+  }
+
+  return false;
+
+})
